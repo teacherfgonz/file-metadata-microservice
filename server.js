bfee13dd51931bae9bb985de1b9d41a3c71d69a9
teacherfgonz <teacherfranciscogonz@gmail.comgit config --global --listgit config --global user.email teacherfranciscogonz@gmail.com>
2021-03-24 20:58:21 -0300

    project done

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -10,6 +12,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }) 
+})
 
 
 
